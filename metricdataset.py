ae7dc3a3480fb2795b93fd308f6d4b1c65a35867
ehsangolshani <ehsanroman74@gmail.com>
2019-10-22 10:09:33 +0330

    implement train and test part, change custom metric dataset and DNN model

--- metricdataset.py
+++ metricdataset.py
@@ -12,10 +12,10 @@ class MetricDataset(Dataset):
              'normalized_gpu_utilization']]
         self.data_tensor: torch.Tensor = torch.tensor(data=self.data.values, dtype=torch.float)
         self.data_tensor = self.data_tensor.contiguous()
+        self.window_size: int = 2
 
     def __getitem__(self, index: int):
-        o = self.data_tensor[index, :]
-        return o
+        return self.data_tensor[index:index + self.window_size, :]
 
     def __len__(self):
-        return self.data_tensor.size()[0]
+        return self.data_tensor.size()[0] - self.window_size + 1
--- model.py
+++ model.py
@@ -2,19 +2,26 @@ from torch import nn
 
 
 class DNN(nn.Module):
-    def __init__(self):
+    def __init__(self, dropout=0.2):
         super(DNN, self).__init__()
-        self.fc1 = nn.Linear(4, 8)
+        self.fc1 = nn.Linear(4, 4)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(8, 8)
+        self.dropout1 = nn.Dropout(p=dropout)
+
+        self.fc2 = nn.Linear(4, 4)
         self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(8, 4)
+        self.dropout2 = nn.Dropout(p=dropout)
+
+        self.fc3 = nn.Linear(4, 4)
         self.relu3 = nn.ReLU()
-        self.fc4 = nn.Linear(4, 1)
+        self.dropout3 = nn.Dropout(p=dropout)
+
+        self.relu_last = nn.ReLU()
+        self.fc_last = nn.Linear(4, 3)
 
     def forward(self, x):
         x = self.relu1(self.fc1(x))
         x = self.relu2(self.fc2(x))
         x = self.relu3(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc_last(x)
         return x
