f2e90fd6afa723533b71bc0c01f71cfd3fa63ac1
ehsangolshani <ehsanroman74@gmail.com>
2019-10-30 09:29:39 +0330

    add cpu_loss, memory_loss and gpu_loss to evaluation

--- run.py
+++ run.py
@@ -18,14 +18,17 @@ test_set_size = int((4 / 10) * len(dataset)) + 1
 
 train_dataset, test_dataset = data.random_split(dataset=dataset, lengths=[train_set_size, test_set_size])
 
-data_loader: data.DataLoader = data.DataLoader(dataset=dataset, batch_size=4, num_workers=4, shuffle=True)
-train_data_loader: data.DataLoader = data.DataLoader(dataset=train_dataset, batch_size=4, num_workers=4, shuffle=True)
-test_data_loader: data.DataLoader = data.DataLoader(dataset=test_dataset, batch_size=4, num_workers=4, shuffle=True)
+data_loader: data.DataLoader = data.DataLoader(dataset=dataset, batch_size=2, num_workers=4, shuffle=True)
+train_data_loader: data.DataLoader = data.DataLoader(dataset=train_dataset, batch_size=2, num_workers=4, shuffle=True)
+test_data_loader: data.DataLoader = data.DataLoader(dataset=test_dataset, batch_size=2, num_workers=4, shuffle=True)
 
 dropout = 0.25
 
 model: DNN = DNN()
 criterion = nn.MSELoss()
+cpu_criterion = nn.MSELoss()
+memory_criterion = nn.MSELoss()
+gpu_criterion = nn.MSELoss()
 optimizer = optim.Adam(params=model.parameters(), lr=1e-3)
 # optimizer = optim.SGD(params=model.parameters(), lr=1e-4, momentum=0.3)
 model.train(mode=True)
@@ -37,8 +40,13 @@ with autograd.detect_anomaly():
             input: torch.Tensor = data[:, 0, :]
             labels: torch.Tensor = data[:, 1, 1:]
             optimizer.zero_grad()
-            outputs = model(input)
-            loss = criterion(outputs, labels)
+            output = model(input)
+
+            loss = criterion(output, labels)
+            cpu_loss = cpu_criterion(output[:, 1], labels[:, 1])
+            memory_loss = cpu_criterion(output[:, 2], labels[:, 2])
+            gpu_loss = cpu_criterion(output[:, 3], labels[:, 3])
+
             loss.backward()
             optimizer.step()
 
@@ -46,7 +54,7 @@ with autograd.detect_anomaly():
             if i % 1000 == 0 and i > 0:
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 500))
-                print('real: ', str(labels), '----- got: ', str(outputs))
+                print('real: ', str(labels), '----- got: ', str(output))
                 print()
                 if (i > 3000 or epoch > 0) and i % 10000 == 0 and (loss < 1.0 or i > 20000):
                     torch.save(model.state_dict(), "model_nasa_dataset" +
@@ -66,11 +74,25 @@ print('start evaluation')
 model.eval()
 
 sum_of_loss = 0
+sum_of_cpu_loss = 0
+sum_of_memory_loss = 0
+sum_of_gpu_loss = 0
 for i, data in enumerate(test_data_loader, 0):
     input: torch.Tensor = data[:, 0, :]
     labels: torch.Tensor = data[:, 1, 1:]
-    outputs = model(input)
-    loss = criterion(outputs, labels)
+    output = model(input)
+
+    loss = criterion(output, labels)
+    cpu_loss = cpu_criterion(output[:, 1], labels[:, 1])
+    memory_loss = cpu_criterion(output[:, 2], labels[:, 2])
+    gpu_loss = cpu_criterion(output[:, 3], labels[:, 3])
+
     sum_of_loss += loss.item()
+    sum_of_cpu_loss += cpu_loss.item()
+    sum_of_memory_loss += memory_loss.item()
+    sum_of_gpu_loss += gpu_loss.item()
 
 print("average total loss: ", sum_of_loss / len(test_data_loader))
+print("average cpu loss: ", sum_of_cpu_loss / len(test_data_loader))
+print("average memory loss: ", sum_of_memory_loss / len(test_data_loader))
+print("average gpu loss: ", sum_of_gpu_loss / len(test_data_loader))
